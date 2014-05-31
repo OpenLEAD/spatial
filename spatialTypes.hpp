4861d36e96c6ade5fc87bbaa50df8b4366e88604
RockStar <rockstar@rockstar-Inspiron-580.(none)>
2014-05-31 18:33:59 -0300

    initial commit by rock-create-orogen

---
+++ spatialTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef spatial_TYPES_HPP
+#define spatial_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace spatial {
+}
+
+#endif
+
